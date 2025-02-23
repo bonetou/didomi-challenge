10fe57c0ba20b4295aef4113a2b079f4f6747414
bonetou <boneto.henrique@gmail.com>
2025-02-23 01:10:18 -0300

    chore: update docker compose to run migrations on startup of application

--- apps/users/src/infrastructure/database/datasource.ts
+++ apps/users/src/infrastructure/database/datasource.ts
@@ -5,7 +5,11 @@ dotenv.config({ path: 'apps/users/.env' });
 
 export const UserDataSource = new DataSource({
   type: 'postgres',
-  url: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [__dirname + '/models/**/*.model{.ts,.js}'],
   migrations: [__dirname + '/migrations/*{.ts,.js}'],
   synchronize: false,
